3824313e6794a4661aba33fb7e0757d2b5e005e8
Stereo <961461851@qq.com>
2022-03-28 22:42:15 +0800

    update premake5

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,16 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+    staticruntime "on"
+	cppdialect "C++17"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	includedirs{
+		".",
+		"../glfw/include",
+	}
 
 	files
 	{
@@ -18,7 +24,11 @@ project "ImGui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_glfw.h",
 	}
 
 	filter "system:windows"
@@ -28,7 +38,7 @@ project "ImGui"
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
+		
 
 	filter "configurations:Debug"
 		runtime "Debug"
